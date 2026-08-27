e4799cfd0b7f94841d5f5cb53f7ee4e286b083fb
Jose Chuque <jchuquec@gmail.com>
2026-08-27 14:13:49 -0500

    "Ejercicio 1 resuelto"

--- Semana 1/Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ Semana 1/Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ejercicio1.ui.theme.Ejercicio1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +28,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             Ejercicio1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TutorialComplete(
+                        header = "Jetpack Compose tutorial",
+                        text1 = "Jetpack Compose is a modern toolkit for building native Android UI. " +
+                                "Compose simplifies and accelerates UI development on Android with less " +
+                                "code, powerful tools, and intuitive Kotlin APIs.",
+                        text2 = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app's UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI's construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +42,47 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Ejercicio1Theme {
-        Greeting("Android")
+        TutorialComplete(
+            header = "Jetpack Compose tutorial",
+            text1 = "Jetpack Compose is a modern toolkit for building native Android UI. " +
+                    "Compose simplifies and accelerates UI development on Android with less " +
+                    "code, powerful tools, and intuitive Kotlin APIs.",
+            text2 = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name."
+        )
+    }
+}
+
+@Composable
+fun TutorialComplete(header: String, text1: String, text2: String, modifier: Modifier = Modifier) {
+    Column(modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Text(
+            text = header,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Text(
+            text = text1,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+
+        Text(
+            text = text2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
